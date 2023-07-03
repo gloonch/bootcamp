57d0337ae6e5e9484b2f4ea6a2a358896f31739d
gloonch <mahdihadian@hotmail.com>
2023-07-03 15:58:06 +0330

    ownership of course and bootcamp

--- seeder.js
+++ seeder.js
@@ -5,6 +5,7 @@ require('dotenv').config({path: './config/config.env'});
 // Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 // Connect to db
 mongoose.connect(process.env.MONGO_URI, {
@@ -15,12 +16,14 @@ mongoose.connect(process.env.MONGO_URI, {
 // read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'));
 
 // Import into the db
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
+        await User.create(users);
         console.log('Data imported from files...');
     } catch (error) {
         console.error(error);
@@ -33,6 +36,7 @@ const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
+        await User.deleteMany();
         console.log('Data deleted using files...');
     } catch (error) {
         console.error(error);
